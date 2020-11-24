db1c86ed8d3179f4dce44ad87a0de40c15c24ed2
HopangLee <hgknim87j@naver.com>
2020-11-24 11:09:23 +0900

    add
    add

---
+++ Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+// 전체적인 게임 진행 관리
+public class GameManager : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public GameObject playerPrefab;
+
+    // 외부에서 싱글톤 오브젝트를 가져올 때 사용할 프로퍼티
+    public static GameManager instance
+    {
+        get
+        {
+            // 만약 싱글톤 변수에 아직 오브젝트가 할당되지 않았다면
+            if (m_instance == null)
+            {
+                // 씬에서 GameManager 오브젝트를 찾아 할당
+                m_instance = FindObjectOfType<GameManager>();
+            }
+
+            // 싱글톤 오브젝트를 반환
+            return m_instance;
+        }
+    }
+
+    private static GameManager m_instance; // 싱글톤이 할당될 static 변수
+
+    private void Awake()
+    {
+        // 씬에 싱글톤 오브젝트가 된 다른 GameManager 오브젝트가 있다면
+        if (instance != this)
+        {
+            // 자신을 파괴
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        float x = 0;
+        float z = 0;
+        Vector3 pos = new Vector3(x, 1f, z);
+
+        // 네트워크 상의 모든 클라이언트들에서 생성 실행
+        // 단, 해당 게임 오브젝트의 주도권은, 생성 메서드를 직접 실행한 클라이언트에게 있음
+        PhotonNetwork.Instantiate(playerPrefab.name, pos, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // 주기적으로 자동 실행되는, 동기화 메서드
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        // 로컬 오브젝트라면 쓰기 부분이 실행됨
+        if (stream.IsWriting)
+        {
+
+        }
+        else
+        {
+            // 리모트 오브젝트라면 읽기 부분이 실행됨
+
+        }
+    }
+}
---
+++ Assets/02.Scripts/PlayerManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+
+/// <summary>
+/// Player manager.
+/// Handles fire Input and Beams.
+/// </summary>
+public class PlayerManager : MonoBehaviourPunCallbacks
+{
+    #region Private Fields
+    private CharacterController controller;
+
+    private float speed = 5f;
+    private float gravity = -9.8f;
+    private Vector3 velocity = new Vector3(0, -2f, 0);
+    #endregion
+
+
+
+    #region MonoBehaviour CallBacks
+
+    /// <summary>
+    /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+    /// </summary>
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    /// <summary>
+    /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+    /// </summary>
+    void Start()
+    {
+        CameraCtrl _cameraCtrl = this.gameObject.GetComponent<CameraCtrl>();
+        if (_cameraCtrl != null)
+        {
+            if (photonView.IsMine)
+            {
+                _cameraCtrl.OnStartFollowing();
+            }
+        }
+        else
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
+        }
+    }
+
+    /// <summary>
+    /// MonoBehaviour method called on GameObject by Unity on every frame.
+    /// </summary>
+    void Update()
+    {
+        // DEBUG test
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+       
+        // 중력
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    #endregion
+
+
+    #region Custom
+
+    #endregion
+}
